544159c4fb6155fcb1a991afdbf217a75103f4c2
Andjela Burns <kaz_ang@yahoo.com>
2024-12-15 22:53:03 +0100

    updated java code

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        int initialBalance = 300; // начальный баланс
-        int refill = 1300; //пополнение счета
+        int initialBalance = 100; // начальный баланс
+        int refill = 1100; //пополнение счета
         int bonus; //бонус
-        if (refill >= 1000){
+        if (refill > 1000) {
             bonus = refill / 100;
         } else {
             bonus = 0;
         }
 
-        String bonusMoney = ((refill / 100) +refill + initialBalance + " " + "рублей");
+        String bonusMoney = (bonus + refill + initialBalance + " " + "рублей");
 
         System.out.println("Подарочный бонус за пополнение счета превышающий 1000 рублей");
         System.out.println("У вас" + " " + (bonusMoney));
