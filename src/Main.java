a1025ec0df623c696fa4137d7dc2ee0111b9e311
Andjela Burns <kaz_ang@yahoo.com>
2024-12-15 22:39:39 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int initialBalance = 300; // начальный баланс
+        int refill = 1300; //пополнение счета
+        int bonus; //бонус
+        if (refill >= 1000){
+            bonus = refill / 100;
+        } else {
+            bonus = 0;
+        }
+
+        String bonusMoney = ((refill / 100) +refill + initialBalance + " " + "рублей");
+
+        System.out.println("Подарочный бонус за пополнение счета превышающий 1000 рублей");
+        System.out.println("У вас" + " " + (bonusMoney));
+
+
+    }
+}
+
